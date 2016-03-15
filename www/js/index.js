dcbbd6cf185a94337c2779a990c7d272e735e9dd
Milan Damen <github@darvit.nl>
2016-03-15 15:53:45 +0100

    Initial commit

---
+++ www/js/index.js
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        app.bindCustomEvents();
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    
+    
+    /**************************************/
+    /*************** Custom ***************/
+    /**************************************/
+    
+    /*** Custom properties ***/
+    
+    
+    
+    /*** Custom methods ***/
+    
+    /**
+     * Binding custom events to page elements
+     */
+    bindCustomEvents: function() {
+        $('#page-list').on('pageshow', function(event) {
+            repo.getChampions(app.parseChampions);
+        });
+    },
+    
+    parseChampions: function(data) {
+        console.log(data);
+        
+        var champList = $('#champlist');
+        var champions = Object.keys(data).sort();
+        champions.forEach(function(champion) {
+            champList.append('<img src="img/champions/' + champion + '.png" alt="' + champion + '" onerror="app.loadAltImage(this)" />');
+        });
+    },
+    
+    loadAltImage: function(el) {
+        el.src = 'http://ddragon.leagueoflegends.com/cdn/6.5.1/img/champion/' + el.alt + '.png';
+    }
+    
+    
+};
---
+++ www/js/repo.js
@@ -0,0 +1,20 @@
+
+var repo = {
+    champions: [],
+    championsListCached: false,
+    
+    getChampions: function(callback) {
+        if (!repo.championsListCached) {
+            // Old link: http://ddragon.leagueoflegends.com/cdn/6.5.1/data/en_US/champion.json
+            
+            // API link: https://developer.riotgames.com/api/methods#!/1055/3633
+            $.get('https://na.api.pvp.net/api/lol/static-data/euw/v1.2/champion?api_key=b725dd4e-3669-4805-8e86-217f53183a2c', function(data) {
+                repo.champions = data.data;
+                callback(data.data);
+                repo.championsListCached = true;
+            });
+        }
+    }
+    
+}
+
